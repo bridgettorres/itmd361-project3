cfd666dbd456c66d97aa0d2e2180032eb830c3e2
Bridget Torres <67389317+bridgettorres@users.noreply.github.com>
2023-04-19 15:16:00 -0500

    finished adding the 3 features
    Added a marker, customized the marker, added a line going from chicago to italy, and added a popup for when the marker gets clicked.

--- js/project3.js
+++ js/project3.js
@@ -1,8 +1,38 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('mapplace'), {
     center: {lat: 42.770503881520774, lng: 12.463473127505198},
-    zoom: 12
+    zoom: 6
   });
+  
+  var marker = new google.maps.Marker({
+    position: {lat: 42.770503881520774, lng: 12.463473127505198},
+    map:map,
+    icon: {
+      url: 'https://www.iconsdb.com/icons/preview/deep-pink/map-marker-2-xxl.png',
+      scaledSize: new google.maps.Size(40, 40)
+    }
+  });
+  
+  var mapText = new google.maps.InfoWindow({
+    content: "Here is where I would like to visit"
+  });
+  
+  marker.addListener('click', function() {
+    mapText.open(map, marker);
+  });
+  
+  var chicago = {lat: 41.88440254314403, lng: -87.62626495557123};
+
+  var line = new google.maps.Polyline({
+    path: [marker.getPosition(), chicago],
+    strokeColor: 'purple',
+    strokeOpacity: 1.0,
+    strokeWeight: 1
+  });
+
+  line.setMap(map);
+  
+  
 }
 
 const gokuimg = document.getElementById('goku');
